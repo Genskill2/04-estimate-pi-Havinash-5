92bc569fceecbc38ac1fb3a9d543fdda7a2e9b60
Havinash-5 <84676591+Havinash-5@users.noreply.github.com>
2021-06-03 12:04:47 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,22 +3,21 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int n);
-
+float mc_pi(int n)
 {
   float x,y;
   int i=0,circle=0;
   float z;
   int square=0;
-  
   float pi;
-  while(i,n-1)
+  
+  while(i<n-1)
   {
   x=frandom();
   y=frandom();
   z=x*x+y*y;
     
-    if(z,=1)
+    if(z<=1)
     {
       circle+=1;
       square+=1;
@@ -29,7 +28,7 @@ float mc_pi(int n);
       i++;
     }
     }
-  pi=(float)4*circle/square;
+  pi=(float)4.0*circle/square;
   return pi;
 }
 
