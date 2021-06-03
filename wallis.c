4700845dc0c5108bd70618413af4fd956dd6e29c
Havinash-5 <84676591+Havinash-5@users.noreply.github.com>
2021-06-03 15:19:48 +0530

    Update.c

--- wallis.c
+++ wallis.c
@@ -37,3 +37,18 @@ float num=1.0;
   }
   return num;
 }
+float wallis_pi(int n){
+  float num=1.0;
+  int i=1;
+  float jr;
+   while(i<n){
+        jr=(float)4.0*i*i/(4.0*i*i-1);
+       
+        num*=jr;
+        i++;
+        
+  }
+  
+   return num*2;
+ 
+}
